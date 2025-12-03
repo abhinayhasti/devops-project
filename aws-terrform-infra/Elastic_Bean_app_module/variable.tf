0bb70c6405d111e5d3f6180ce7a75d32daa6c718
abhinayhasti <abhinayhasti46@gmail.com>
2025-12-03 17:30:49 -0500

    elastic beaanstalk application provisioned

---
+++ aws-terrform-infra/Elastic_Bean_app_module/variable.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  description = "Access key to AWS console"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "Secret key to AWS console"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
