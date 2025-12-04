3d0e9676f1a60efac82fb43b42ae59bac6451e15
abhinayhasti <abhinayhasti46@gmail.com>
2025-12-03 23:55:25 -0500

    event bridge rule on ec2 instance updated

---
+++ aws-terrform-infra/Eventbridge_Rule_EC2_module/variable.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+  description = "Access key to AWS console"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "Secret key to AWS console"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "endpoint" {
+  description = "SNS topic subscription endpoint (email address)"
+  type        = string
+}
